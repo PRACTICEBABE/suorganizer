ff07afc2e31939adfa974a376c5f13f6c5498be9
PRACTICEBABE <85937206+PRACTICEBABE@users.noreply.github.com>
2021-06-15 17:06:07 +0530

    inittial

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from organizer.models import Tag, Startup, 
+
+# Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=63)
+	slug = models.SlugField()
+	text = models.TextField()
+	pub_date = models.DateField()
+	tags = models.ManyToManyField(Tag)
+	startup = models.ManyToManyField(Startup)
