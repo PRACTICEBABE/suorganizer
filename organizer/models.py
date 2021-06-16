650de4488dd0dc00b23966df7e1c5853757e488d
PRACTICEBABE <85937206+PRACTICEBABE@users.noreply.github.com>
2021-06-16 16:03:41 +0530

    chapter 3 end

--- organizer/models.py
+++ organizer/models.py
@@ -7,17 +7,46 @@ class Tag(models.Model):
 	name = models.CharField(max_length=31)
 	slug = models.SlugField()
 
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
+
+
+
 class Startup(models.Model):
-	name = models.CharField(max_length=31)
-	slug = models.SlugField()
+	name = models.CharField(
+		max_length=31,
+		db_index=True
+		)
+	slug = models.SlugField(
+		max_length=31,
+		unique=True,
+		help_text='A label for URL config.')
 	description = models.TextField()
-	founded_date = models.DateField()
+	founded_date = models.DateField('date founded')
 	contact = models.EmailField()
-	website = models.URLField()
+	website = models.URLField(max_length=255)
 	tags = models.ManyToManyField(Tag)
+
+	class Meta:
+		ordering = ['name']
+		get_latest_by = 'founded_date'
+
+	def __str__(self):
+		return self.name
 
 class NewsLink(models.Model):
 	name = models.CharField(max_length=63)
 	pub_date = models.DateField()
 	link = models.URLField()
-	startup = models.ForeignKey(Startup)
+	startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
+
+	class Meta:
+		verbose_name = 'News Article'
+		ordering = ['-pub_date']
+		get_latest_by = 'pub_date'
+
+	def __str__(self):
+		return f"{self.startup} {self.name}"
